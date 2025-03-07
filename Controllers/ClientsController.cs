3ecd6bd5008f2b7bdb12cf8e2e9c3408487a8175
Vladyslav Tarasov <tarasovvlad137@gmail.com>
2025-03-07 23:44:42 +0100

    Added a controller and a view for clients with a function to add new clients

---
+++ Controllers/ClientsController.cs
@@ -0,0 +1,37 @@
+﻿using CarRentalApp_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRentalApp_MVC.Controllers
+{
+    public class ClientsController : Controller
+    {
+        private readonly RentalContext _context;
+
+        public ClientsController(RentalContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind("ClientId, FirstName, LastName, DriversLicenseNumber, PhoneNumber, Email, Status")] Client client)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Add(client);
+                await _context.SaveChangesAsync();
+                return View(nameof(Index));
+            }
+            return View(client);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
