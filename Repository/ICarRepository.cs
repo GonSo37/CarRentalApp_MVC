2f9721c1dda2fba6543fdde6edf7c65899fdff95
Vladyslav Tarasov <tarasovvlad137@gmail.com>
2025-03-24 15:42:30 +0100

    Partially added CRUD for the cars table

---
+++ Repository/ICarRepository.cs
@@ -0,0 +1,14 @@
+﻿using CarRentalApp_MVC.Models;
+
+namespace CarRentalApp_MVC.Repository
+{
+    public interface ICarRepository
+    {
+        IQueryable<Car> GetAll();
+        Car GetById(int CarID);
+        void Insert(Car car);
+        void Update(Car car);
+        void Delete(int CarID);
+        void Save();
+    }
+}
