14fb45db5a6c245496e23f791f4dccbc9360dd54
Vladyslav Tarasov <tarasovvlad137@gmail.com>
2025-03-25 21:25:32 +0100

    CRUD logic and a page to display all rentals have been added

---
+++ Repository/IRentalRepository.cs
@@ -0,0 +1,14 @@
+﻿using CarRentalApp_MVC.Models;
+
+namespace CarRentalApp_MVC.Repository
+{
+    public interface IRentalRepository
+    {
+        IQueryable<Rental> GetAll();
+        Rental GetById(int RentalID);
+        void Insert(Rental rental);
+        void Update(Rental rental);
+        void Delete(int RentalID);
+        void Save();
+    }
+}
---
+++ Repository/RentalRepository.cs
@@ -0,0 +1,65 @@
+﻿using CarRentalApp_MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarRentalApp_MVC.Repository
+{
+    public class RentalRepository : IRentalRepository
+    {
+        private readonly RentalContext _context;
+
+        public RentalRepository(RentalContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<Rental> GetAll()
+        {
+            return _context.Rentals.Include(r => r.Car)
+                .Include(r => r.Client);
+        }
+        public Rental GetById(int RentalID)
+        {
+            return _context.Rentals.Find(RentalID);
+        }
+        public void Insert(Rental rental)
+        {
+            _context.Rentals.Add(rental);
+        }
+        public void Update(Rental rental)
+        {
+            _context.Entry(rental).State = EntityState.Modified;
+        }
+        public void Delete(int RentalID)
+        {
+            Rental rental = _context.Rentals.Find(RentalID);
+
+            if (rental != null)
+            {
+                _context.Rentals.Remove(rental);
+            }
+        }
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+
+        private bool disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
