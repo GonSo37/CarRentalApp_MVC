9a256c02836bdd58d00a3d3d0297a455c4cf620a
Vladyslav Tarasov <tarasovvlad137@gmail.com>
2025-04-03 18:14:31 +0200

    Added ViewModel layer for all tables

--- Controllers/ClientsController.cs
+++ Controllers/ClientsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using CarRentalApp_MVC.Repository;
 using CarRentalApp_MVC.Services;
+using CarRentalApp_MVC.ViewModels;
 
 namespace CarRentalApp_MVC.Controllers
 {
@@ -18,23 +19,43 @@ namespace CarRentalApp_MVC.Controllers
         [HttpGet]
         public ActionResult Index()
         {
-            var model = _clientService.GetAllClients();
+            var clients = _clientService.GetAllClients();
+            var model = clients.Select(client => new ClientViewModel
+            {
+                ClientId = client.ClientId,
+                FirstName = client.FirstName,
+                LastName = client.LastName,
+                DriversLicenseNumber = client.DriversLicenseNumber,
+                PhoneNumber = client.PhoneNumber,
+                Email = client.Email,
+                Status = client.Status
+            });
             return View(model);
         }
 
 
         public IActionResult AddClient()
         {
-            return View();
+            return View(new ClientViewModel());
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult AddClient(Client model)
+        public ActionResult AddClient(ClientViewModel model)
         {
             if (ModelState.IsValid)
             {
-                _clientService.AddClient(model);
+                var client = new Client
+                {
+                    ClientId = model.ClientId,
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    DriversLicenseNumber = model.DriversLicenseNumber,
+                    PhoneNumber = model.PhoneNumber,
+                    Email = model.Email,
+                    Status = model.Status
+                };
+                _clientService.AddClient(client);
                 _clientService.Save();
                 return RedirectToAction("Index", "Clients");
             }
@@ -44,16 +65,36 @@ namespace CarRentalApp_MVC.Controllers
         [HttpGet]
         public ActionResult EditClient(int ClientId)
         {
-            Client model = _clientService.GetClientById(ClientId);
+            var client = _clientService.GetClientById(ClientId);
+            var model = new ClientViewModel
+            {
+                ClientId = client.ClientId,
+                FirstName = client.FirstName,
+                LastName = client.LastName,
+                DriversLicenseNumber = client.DriversLicenseNumber,
+                PhoneNumber = client.PhoneNumber,
+                Email = client.Email,
+                Status = client.Status
+            };
             return View(model);
         }
 
         [HttpPost]
-        public ActionResult EditClient(Client model)
+        public ActionResult EditClient(ClientViewModel model)
         {
             if (ModelState.IsValid)
             {
-                _clientService.UpdateClient(model);
+                var client = new Client
+                {
+                    ClientId = model.ClientId,
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    DriversLicenseNumber = model.DriversLicenseNumber,
+                    PhoneNumber = model.PhoneNumber,
+                    Email = model.Email,
+                    Status = model.Status
+                };
+                _clientService.UpdateClient(client);
                 _clientService.Save();
                 return RedirectToAction("Index", "Clients");
             }
@@ -66,7 +107,17 @@ namespace CarRentalApp_MVC.Controllers
         [HttpGet]
         public ActionResult DeleteClient(int ClientID)
         {
-            Client model = _clientService.GetClientById(ClientID);
+            var client = _clientService.GetClientById(ClientID);
+            var model = new ClientViewModel
+            {
+                ClientId = client.ClientId,
+                FirstName = client.FirstName,
+                LastName = client.LastName,
+                DriversLicenseNumber = client.DriversLicenseNumber,
+                PhoneNumber = client.PhoneNumber,
+                Email = client.Email,
+                Status = client.Status
+            };
             return View(model);
         }
 
---
+++ ViewModels/ClientViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace CarRentalApp_MVC.ViewModels
+{
+    public class ClientViewModel
+    {
+        public int ClientId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DriversLicenseNumber { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Status { get; set; }
+    }
+}
