f08bbab76b99e826e235f45b7a3a40da8ee59a7b
Vladyslav Tarasov <tarasovvlad137@gmail.com>
2025-05-08 17:46:02 +0200

    Identity was added to the project

--- Migrations/20250508144509_AddPricePerDayColumn.Designer.cs
+++ Migrations/20250508144509_AddPricePerDayColumn.Designer.cs
@@ -4,6 +4,7 @@ using CarRentalApp_MVC.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CarRentalApp_MVC.Migrations
 {
     [DbContext(typeof(RentalContext))]
-    partial class RentalContextModelSnapshot : ModelSnapshot
+    [Migration("20250508144509_AddPricePerDayColumn")]
+    partial class AddPricePerDayColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
