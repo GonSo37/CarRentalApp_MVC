5b8e9f0521f4793c3be6215e8477b902f4f42051
Vladyslav Tarasov <tarasovvlad137@gmail.com>
2025-05-24 17:39:58 +0200

    The design of the admin panel has been updated and the functionality to add new users has been implemented

---
+++ ViewModels/CreateUserViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarRentalApp_MVC.ViewModels
+{
+    public class CreateUserViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        public string Role { get; set; }
+    }
+}
